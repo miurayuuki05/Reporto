468699be01eb6cde67b696b1bd4ee0504f9f707e
miurayuuki05 <lolipopfaiz0@gmail.com>
2025-01-03 13:05:42 +0700

    Fix dbcontext

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -4,11 +4,11 @@ namespace Reporto.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<DbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Report> Reports { get; set; }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Report>? Reports { get; set; }
     }
 
     public class User
